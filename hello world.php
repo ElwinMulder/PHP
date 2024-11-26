f6793d480da1ddee476ea95cb679dd25fb47b340
ElwinMulder <9025151@student.zadkine.nl>
2024-11-26 20:52:47 +0100

    Add files via upload

---
+++ hello world.php
@@ -0,0 +1,21 @@
+<?php
+// gemaakt door Elwin Mulder
+echo  "<h1>Hallo wereld,</h1>";
+echo "ik zit op het techniek college";
+//
+$schoolnaam = "Techniek college Rotterdam!";
+//
+echo $ElwinMulder;
+?>
+
+<?php
+//
+echo  "<h1>Hallo wereld,</h1>";
+echo "ik zit op het techniek college";
+//
+$schoolnaam = "Techniek college Rotterdam!";
+//
+echo $Techniekcollerotterdam;
+//
+echo $Extraaregel;
+?>
